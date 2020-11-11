a92669a10a2f4254de668dc7de11881d0a09123c
Chloe Deng <chloedeng0309@gmail.com>
2020-11-11 14:50:16 -0800

    First working dashboard

---
+++ app.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+import datetime
+import pandas as pd
+import numpy as np
+import processData
+import os
+import pdb
+import glog
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_table
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+
+import plotly.express as px
+from ipywidgets import widgets
+
+prev_day = datetime.datetime.today() - datetime.timedelta(1)
+dateStr = '{}-{}-{:02d}'.format(prev_day.year, prev_day.month, prev_day.day)
+usStateDataLink = "9mfq-cb36"
+usDeathDataLink = '9bhg-hcku'
+caseSurveillanceData = 'vbim-akqf'
+
+us_state_data_query = "SELECT * WHERE submission_date="
+case_surveillance_query = "SELECT * WHERE cdc_report_dt="
+
+processData.check_download(
+    prev_day,
+    usStateDataLink,
+    us_state_data_query,
+    'US-State-Data',
+    'StateData')
+processData.check_download(
+    prev_day,
+    caseSurveillanceData,
+    case_surveillance_query,
+    'Case-Surveillance',
+    'CaseSurveillanceData')
+
+usDataDf = processData.consolidate_state_data()
+usDataDf['Date'] = usDataDf['Date'].apply(pd.to_datetime)
+usDataDf['submission_date'] = usDataDf['submission_date'].apply(pd.to_datetime)
+
+external_scripts = ['/assets/style.css']
+
+usDataDf2 = usDataDf[~usDataDf.state.isin(['US'])]
+# usDataDf = pd.read_csv('./ProdData/USDatabyStates.csv')
+# currDate = datetime.dateime(2020,10,24)
+
+topCases = usDataDf2[usDataDf2['Date'] == dateStr][['Date',
+                                                             'state',
+                                                             'tot_cases',
+                                                             'tot_death']].sort_values(
+    'tot_cases', ascending=False)
+USTotalCases = usDataDf2[usDataDf2['submission_date']
+    == dateStr]['tot_cases'].astype(float).sum()
+statesNames = usDataDf.sort_values('state')['state'].unique().tolist()
+usDataDf['new_case'] = usDataDf['new_case'].astype(float)
+# pdb.set_trace()
+USTopNewCases = usDataDf2[usDataDf2['submission_date'] == dateStr][['state', 'new_case']].sort_values(
+    'new_case', ascending=False)
+# pdb.set_trace()
+# tot_cases_growth = usDataDf.pivot_table(
+#     'tot_cases',
+#     ['Date'],
+#     'state').sort_values('Date').pct_change(
+#         fill_method='pad')
+
+death_rate_rank = usDataDf[usDataDf['Date'] == dateStr].sort_values('death rate',
+                                                                         ascending=False, na_position='last')[['state', 'death rate']]
+
+usDataDf['submission_date'] = pd.to_datetime(
+    usDataDf['submission_date']).apply(
+        lambda x: x.date())
+
+us_cases = usDataDf.copy()
+for col in us_cases.columns:
+    us_cases[col] = us_cases[col].astype(str)
+
+us_cases = us_cases[~us_cases.state.isin(['US'])][['Date', 'submission_date', 'state',
+                                                   'tot_cases', 'tot_death', 'new_case', 'new_death']]
+us_cases['Data'] = (us_cases['state'] + '<br>' +
+                    'Total Cases ' + us_cases['tot_cases'] + '<br>' +
+                    'Total Death ' + us_cases['tot_death'] + '<br>' +
+                    'New Case ' + us_cases['new_case'] + '<br>' +
+                    'New Death ' + us_cases['new_death'])
+glog.info('Plot animcation')
+
+us_cases['tot_cases'] = us_cases['tot_cases'].astype(float).astype(int)
+
+fig = px.choropleth(us_cases,
+                    scope='usa',
+                    locations="state",
+                    locationmode='USA-states',
+                    color=us_cases['tot_cases'],
+                    hover_name="state",
+                    featureidkey='properties.state',
+                    hover_data=['Data'],
+                    animation_frame="submission_date", animation_group="state",
+                    color_continuous_scale='Reds',
+                    labels={'tot_cases': 'Total Number of Cases'},
+                    title='US Covid Cases'
+                    )
+
+fig.update_layout(
+    showlegend=True,
+    legend_title_text='<b>Total Number of Cases</b>',
+    font={"size": 16, "color": "#808080", "family": "calibri"},
+    margin={"r": 0, "t": 40, "l": 0, "b": 0},
+    legend=dict(orientation='v'),
+    geo=dict(bgcolor='rgba(0,0,0,0)', lakecolor='#e0fffe')
+)
+
+data = usDataDf2[usDataDf2['Date'] == dateStr]
+
+fig2 = px.choropleth(data,
+                    scope='usa',
+                    locations="state",
+                    locationmode='USA-states',
+                    color=data['Risk Level'],
+                    hover_name="state",
+                    featureidkey='properties.state',
+                    hover_data=['7 day average new cases with 7 day lag'],
+                    # animation_frame="submission_date", animation_group="state",
+                    color_continuous_scale='Reds',
+                    labels={
+    'Risk Level': 'Risk level based on 7 day average new cases with 7 day lag'},
+                    title='State Risk Level'
+                    )
+
+fig2.update_layout(
+    showlegend=True,
+    legend_title_text='<b>Risk Level</b>',
+    font={"size": 16, "color": "#808080", "family": "calibri"},
+    margin={"r": 0, "t": 40, "l": 0, "b": 0},
+    legend=dict(orientation='v'),
+    geo=dict(bgcolor='rgba(0,0,0,0)', lakecolor='#e0fffe')
+)
+
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUMEN])
+# app = dash.Dash(__name__, external_stylesheets=external_scripts)
+
+app.layout = html.Div([html.H1("US Covid Data Dashboard", style={"textAlign": "center"}),
+                       dcc.Markdown(
+    '''US covid data visualization using CDC public data''', style={
+        "textAlign": "center"}),
+
+    dcc.Tabs(id="tabs", children=[
+        dcc.Tab(label='Daily Summary',
+                children=[html.Div([html.H2("As of {} US Total Reported Cases:  {}".format(
+                                            dateStr, USTotalCases),
+                                            style={'textAlign': 'center', "margin-left": "50px",
+                                                   'marginBottom': 30, 'marginTop': 30}
+                                            ),
+                                    html.H4("Top 5 States with the Most New Cases Today",
+                                            style={'textAlign': 'left', "margin-left": "50px",
+                                                   'marginBottom': 30, 'marginTop': 30}),
+                                    dash_table.DataTable(id='table2',
+                                                         columns=[{"name": i, "id": i}
+                                                                  for i in USTopNewCases.columns],
+                                                         data=USTopNewCases.iloc[0:5, :].to_dict(
+                                                             "rows"),
+                                                         style_header={'backgroundColor': 'rgb(230, 230, 230)',
+                                                                       'fontWeight': 'bold'},
+                                                         style_cell={
+                                                             'textAlign': 'center'},
+                                                         style_data={"margin-left": "auto",
+                                                                     "margin-right": "auto"}
+                                                         ),
+                                    html.H3("Top 5 States with the Highest Death Rate",
+                                            style={'textAlign': 'left', "margin-left": "50px",
+                                                   'marginBottom': 30, 'marginTop': 30}),
+                                    dash_table.DataTable(id='table3',
+                                                         columns=[{"name": i, "id": i}
+                                                                  for i in death_rate_rank.columns],
+                                                         data=death_rate_rank.iloc[0:5, :].to_dict(
+                                                             "rows"),
+                                                         style_header={'backgroundColor': 'rgb(230, 230, 230)',
+                                                                       'fontWeight': 'bold'},
+                                                         style_cell={
+                                                             'textAlign': 'center'},
+                                                         style_data={"margin-left": "auto",
+                                                                     "margin-right": "auto"}
+                                                         ),
+                                    html.H3("Covid Risk Level",
+                                    style={"textAlign": "left", 'marginBottom': 30, 'marginTop': 30}),
+                                    dcc.Graph(figure=fig2)
+
+                                    ])]
+                ),
+
+
+        # First Tab
+        dcc.Tab(label='US Total Cases',
+                children=[html.Div([html.H3("Top 5 States by Total Covid Cases", style={'textAlign': 'left',
+                'marginBottom': 100, 'marginTop': 100}),
+                                    dash_table.DataTable(id='table',
+                                                         columns=[{"name": i, "id": i}
+                                                                  for i in topCases.columns],
+                                                         data=topCases.iloc[0:5, :].to_dict(
+                                                             "rows"),
+                                                         style_header={'backgroundColor': 'rgb(230, 230, 230)',
+                                                                       'fontWeight': 'bold'},
+                                                         style_cell={
+                                                             'textAlign': 'center'},
+                                                         style_data={"margin-left": "auto",
+                                                                     "margin-right": "auto"},
+                                                         ),
+                                    html.H3(
+                    "Covid Cases by States Animation", style={
+                        "textAlign": "left", 'marginBottom': 100, 'marginTop': 100}),
+                    dcc.Graph(figure=fig)])]
+                ),
+
+        # Second Tab
+        dcc.Tab(label='US Death Rate',
+                children=[html.Div([html.H1("US Death Rate by States", style={"textAlign": "center"}),
+                                    html.Div([html.Div([dcc.Dropdown(id='state-selected',
+                                                                     value='state',
+                                                                     options=[{'label': i, 'value': i} for i in statesNames])],
+                                                       style={"display": "block",
+                                                              "margin-left": "auto",
+                                                              "margin-right": "auto",
+                                                              "width": "60%"}),
+                                              ],),
+                                    dcc.Graph(id='us_death_rate')
+
+                                    ])]),
+
+        # Third Tab
+        dcc.Tab(label='US Case Surveillance')
+    ])
+])
+
+
+@ app.callback(Output('us_death_rate', 'figure'),
+              [Input('state-selected', 'value')])
+def update_graph(selected_dropdown):
+
+    chart_title='State Death Rate Time Series: {}'.format(selected_dropdown)
+    figure=px.line(usDataDf[usDataDf['state'] == selected_dropdown],
+                     x = "submission_date",
+                     y = "death rate",
+                     title= chart_title)
+
+    return figure
+
+
+if __name__ == '__main__':
+    app.run_server(debug= True)
---
+++ processData.py
@@ -0,0 +1,99 @@
+import datetime
+import pandas as pd
+import numpy as np
+import os
+from sodapy import Socrata
+import glog
+import pdb
+
+usStateDataLink="9mfq-cb36"
+usDeathDataLink = '9bhg-hcku'
+caseSurveillanceData = 'vbim-akqf'
+
+us_state_data_query= "SELECT * WHERE submission_date="
+case_surveillance_query="SELECT * WHERE cdc_report_dt="
+
+def downloadFile(date, filesource, dataquery,filenamePrefix, folder):
+    """Download data from CDC website"""
+
+    client = Socrata("data.cdc.gov", None)
+    results = client.get(filesource, query=dataquery)
+    results_df = pd.DataFrame.from_records(results)
+    filename = './{}/{}-{}.csv'.format(folder,filenamePrefix,date)
+    results_df.to_csv(filename)
+
+def processPopData(input_file):
+    """function to convert wikipedia us population data"""
+
+    df = pd.read_html(input_file)
+    newdf = df[0][['State','Population estimate, July 1, 2019[2]']]
+    newdf.columns=['State','Population']
+    code = pd.read_csv('StateCode.csv')
+    merge = pd.merge(left=newdf, right=code, left_on='State', right_on='Name', how='outer')
+    merge.loc[merge['State']=='U.S. Virgin Islands','State or Region Code']='VI'
+    merge.loc[merge['State']=='District of Columbia','State or Region Code']='DC'
+    merge.loc[merge['State']=='Total U.S. (including D.C. and territories)','State or Region Code']='US'
+    output = merge[(merge['State or Region Code'].isnull()==False)&(merge['Population'].isnull()==False)][['State or Region Code','Population']]
+
+    output.to_csv('./data/US_Population.csv')
+
+    return output
+
+def check_download(date, filesource, dataquery, filenamePrefix, folder):
+    """Daily checker to see how if files have been updated on CDC website"""
+
+    #Download State Data
+    daterange = pd.date_range('2020-01-24',date)
+    # for file in os.listdir(folder):
+    #     print(file.split('.')[0][-10:])
+    #     pdb.set_trace()
+    #     print(datetime.datetime.strptime(file[-10:], '%Y-%m-%d'))
+    datelist = [datetime.datetime.strptime(file.split('.')[0][-10:], '%Y-%m-%d')  for file in os.listdir(folder)]
+    missing_dates = [d for d in daterange if d not in datelist]
+    glog.info('Missing dates {}'.format(missing_dates))
+    for dt in missing_dates:
+        query = "{}'{}'".format(dataquery, dt.date())
+        downloadFile(dt.date(), filesource, query, filenamePrefix, folder)
+    glog.info('Done Downloading files')
+
+def consolidate_state_data():
+    """combine daily files to one consolidated file """
+
+    path = './StateData'
+    usDataDf = pd.DataFrame(columns=['Date','state','tot_cases', 'new_case','tot_death','new_death'])
+    for file in os.listdir('./StateData'):
+        dt = os.path.basename(file).split('.')[0][-10:]
+        df = pd.read_csv(os.path.join(path, file))
+        df['Date'] = dt
+        df[['Date','submission_date']] = df[['Date','submission_date']].apply(pd.to_datetime)
+        df[['tot_cases', 'new_case','tot_death','new_death']] = df[['tot_cases', 'new_case','tot_death','new_death']].apply(pd.to_numeric)
+        df.loc['US'] = df.sum(numeric_only=True)
+        df.loc['US','state']='US'
+        df.loc['US','Date'] = dt
+        df.loc['US','submission_date']=dt
+        usDataDf=usDataDf.append(df[['Date','submission_date','state','tot_cases', 'new_case','tot_death','new_death']])
+
+    usDataDf['death rate'] = usDataDf['tot_death'].astype(float)/usDataDf['tot_cases'].astype(float)
+    usDataDf['death rate']=usDataDf['death rate'].fillna(0)
+
+    pop = processPopData('./data/pop.html')
+    newrow = {'State or Region Code':'NYC', 'Population':8336817}
+    pop=pop.append(newrow, ignore_index=True)
+    test = pd.merge(left=usDataDf, right=pop, left_on='state',right_on='State or Region Code', how='outer')
+    test['Total Cases per Population'] = test['tot_cases']/test['Population']
+    test['New Cases per Population'] = test['new_case']/test['Population']
+    test['7 day average new cases with 7 day lag']=100000*(test['new_case'].shift(7).rolling(window=7).mean())/test['Population']
+    col='7 day average new cases with 7 day lag'
+    conditions = [(test[col]<1.0),
+                  (test[col]>=1.0) & (test[col]<4.0),
+                  (test[col]>=4) & (test[col]<7),
+                  (test[col]>=7)
+                  ]
+    values = ['Minimal Tier 4','Moderate Tier 3','Substantial Tier 2','Widespread Tier 1']
+    test['Risk Level'] = np.select(conditions, values)
+    test = test.dropna(subset=['Population','State or Region Code'])
+    
+    test.to_csv('./ProdData/USDatabyStates.csv')
+
+
+    return test
