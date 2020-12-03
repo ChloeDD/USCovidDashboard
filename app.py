3b9c88348e5c851d9976af93685a70725d81e5ae
Chloe Deng <chloedeng0309@gmail.com>
2020-12-02 23:01:30 -0800

    Prepare for heroku

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 import os
 import pandas as pd
 import processData
-import pdb
 
 import dash
 import dash_core_components as dcc
@@ -13,11 +12,8 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_table
 from dash.dependencies import Input, Output
-
 import plotly.express as px
-import plotly.graph_objs as go
 
-from ipywidgets import widgets
 
 prev_day = datetime.datetime.today() - datetime.timedelta(1)
 
